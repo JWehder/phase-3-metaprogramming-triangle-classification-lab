9ceea16b7c484ed150673e1f11c466105b983412
Jake Wehder <jw@email.comjake>
2023-02-02 13:25:20 -0600

    commit tree b1c0b50af59acdce35ced3e6d4f0a0016a41d47e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b = b 
+    @c = c
+  end
+
+  def sides_positive?
+    [@a, @b, @c].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    @a + @b > @c && @a + @c > @b && @c + @b > @a 
+  end
+
+  def valid_triangle?
+    raise TriangleError unless triangle_inequality? && sides_positive?
+  end
+
+  def kind 
+    valid_triangle?
+    if (@a == @b && @b == @c) 
+      :equilateral
+    elsif (@a == @b || @a == @c || @b == @c)
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
